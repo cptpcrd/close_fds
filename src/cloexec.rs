6de35f3a03dcc033e454b3202ea3c3ddfd27a14b
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2021-02-26 08:38:52 -0500

    refactor: use close_range() more in set_fds_cloexec()
    This is similar to the closefrom()/close_range() optimization in
    close_open_fds() that can kick in once all the file descriptors in
    keep_fds have been passed.
    
    It allows still using close_range() for optimizations if keep_fds isn't
    sorted.

--- src/cloexec.rs
+++ src/cloexec.rs
@@ -1,5 +1,51 @@
 use crate::{fditer, sys, util};
 
+#[cfg(target_os = "linux")]
+use core::sync::atomic::{AtomicBool, Ordering};
+
+#[cfg(target_os = "linux")]
+static mut MAY_HAVE_CLOSE_RANGE_CLOEXEC: AtomicBool = AtomicBool::new(true);
+
+#[cfg(target_os = "linux")]
+#[inline]
+unsafe fn set_cloexec_range(minfd: libc::c_uint, maxfd: libc::c_uint) -> Result<(), ()> {
+    debug_assert!(minfd <= maxfd, "{} > {}", minfd, maxfd);
+
+    if libc::syscall(
+        sys::SYS_CLOSE_RANGE,
+        minfd as libc::c_uint,
+        maxfd as libc::c_uint,
+        sys::CLOSE_RANGE_CLOEXEC,
+    ) == 0
+    {
+        Ok(())
+    } else {
+        MAY_HAVE_CLOSE_RANGE_CLOEXEC.store(false, Ordering::Relaxed);
+        Err(())
+    }
+}
+
+#[cfg(target_os = "linux")]
+#[inline]
+unsafe fn set_cloexec_shortcut(
+    minfd: libc::c_int,
+    keep_fds: &[libc::c_int],
+    max_keep_fd: libc::c_int,
+    fds_sorted: bool,
+) -> Result<(), ()> {
+    if !unsafe { MAY_HAVE_CLOSE_RANGE_CLOEXEC.load(Ordering::Relaxed) } {
+        Err(())
+    } else if max_keep_fd < minfd {
+        set_cloexec_range(minfd as libc::c_uint, libc::c_uint::MAX)
+    } else if fds_sorted {
+        util::apply_range(minfd, keep_fds, |low, high| {
+            set_cloexec_range(low as libc::c_uint, high as libc::c_uint)
+        })
+    } else {
+        Err(())
+    }
+}
+
 pub fn set_fds_cloexec_generic(
     mut minfd: libc::c_int,
     mut keep_fds: &[libc::c_int],
@@ -12,55 +58,25 @@ pub fn set_fds_cloexec_generic(
     keep_fds = util::simplify_keep_fds(keep_fds, fds_sorted, &mut minfd);
 
     #[cfg(target_os = "linux")]
-    {
-        use core::sync::atomic::{AtomicBool, Ordering};
-        static mut MAY_HAVE_CLOSE_RANGE_CLOEXEC: AtomicBool = AtomicBool::new(true);
-
-        #[inline]
-        unsafe fn set_cloexec_range(minfd: libc::c_uint, maxfd: libc::c_uint) -> Result<(), ()> {
-            debug_assert!(minfd <= maxfd, "{} > {}", minfd, maxfd);
-
-            if libc::syscall(
-                sys::SYS_CLOSE_RANGE,
-                minfd as libc::c_uint,
-                maxfd as libc::c_uint,
-                sys::CLOSE_RANGE_CLOEXEC,
-            ) == 0
-            {
-                Ok(())
-            } else {
-                MAY_HAVE_CLOSE_RANGE_CLOEXEC.store(false, Ordering::Relaxed);
-                Err(())
-            }
-        }
-
-        #[inline]
-        unsafe fn set_cloexec_shortcut(
-            minfd: libc::c_int,
-            keep_fds: &[libc::c_int],
-            max_keep_fd: libc::c_int,
-            fds_sorted: bool,
-        ) -> Result<(), ()> {
-            if max_keep_fd < minfd {
-                set_cloexec_range(minfd as libc::c_uint, libc::c_uint::MAX)
-            } else if fds_sorted {
-                util::apply_range(minfd, keep_fds, |low, high| {
-                    set_cloexec_range(low as libc::c_uint, high as libc::c_uint)
-                })
-            } else {
-                Err(())
-            }
-        }
-
-        if unsafe { MAY_HAVE_CLOSE_RANGE_CLOEXEC.load(Ordering::Relaxed) }
-            && unsafe { set_cloexec_shortcut(minfd, keep_fds, max_keep_fd, fds_sorted) }.is_ok()
-        {
-            return;
-        }
+    if unsafe { set_cloexec_shortcut(minfd, keep_fds, max_keep_fd, fds_sorted) }.is_ok() {
+        return;
     }
 
     for fd in fditer::iter_fds(minfd, true, thread_safe) {
-        if fd > max_keep_fd || !util::check_should_keep(&mut keep_fds, fd, fds_sorted) {
+        if fd > max_keep_fd {
+            // We know that none of the file descriptors we encounter from here onward can be in
+            // keep_fds.
+
+            #[cfg(target_os = "linux")]
+            if unsafe { MAY_HAVE_CLOSE_RANGE_CLOEXEC.load(Ordering::Relaxed) }
+                && unsafe { set_cloexec_range(fd as libc::c_uint, libc::c_uint::MAX).is_ok() }
+            {
+                return;
+            }
+
+            // Otherwise, this just lets us skip calling check_should_keep()
+            util::set_cloexec(fd);
+        } else if !util::check_should_keep(&mut keep_fds, fd, fds_sorted) {
             // It's not in keep_fds
             util::set_cloexec(fd);
         }
