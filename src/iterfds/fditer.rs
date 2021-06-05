091b13713940407829f5a0bd5ba8c4def9920e19
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2021-06-05 13:22:56 -0400

    fix(fditer): fix maxfd=-1 handling in size_hint()

--- src/iterfds/fditer.rs
+++ src/iterfds/fditer.rs
@@ -254,6 +254,12 @@ impl Iterator for FdIter {
         }
 
         if let Some(maxfd) = self.maxfd {
+            if maxfd == -1 {
+                // No file descriptors open
+                return (0, Some(0));
+            }
+            debug_assert!(maxfd > 0);
+
             // maxfd is set; we can give an upper bound by comparing to curfd
             let diff = (maxfd as usize + 1).saturating_sub(self.curfd as usize);
 
