bf660c489cf7d7bd50c24c263d8596d78d895dbe
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2021-06-26 09:34:12 -0400

    refactor: fix integer cast in close_range() syscall

--- src/closefds/close.rs
+++ src/closefds/close.rs
@@ -175,7 +175,7 @@ unsafe fn try_close_range(minfd: libc::c_uint, maxfd: libc::c_uint) -> Result<()
         crate::sys::SYS_CLOSE_RANGE,
         minfd as libc::c_uint,
         maxfd as libc::c_uint,
-        0,
+        0 as libc::c_uint,
     ) == 0
     {
         Ok(())
